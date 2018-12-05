d9a3b6fcd2021804bcaa6f23163b773fc6c2c8e4
mdpatel4 <31790430+mdpatel4@users.noreply.github.com>
2018-12-04 23:04:52 -0800

    lambdaToDb.js
    update the code for reading two tables at once.

--- website/lambdaToDb.js
+++ website/lambdaToDb.js
@@ -1,43 +1,31 @@
-/*
-Reading database information from dynamoDB to Lambda function
-created by Maulik Patel
+/**
+This is lambda function code. The lambda function takes place between Dynamodb and APIGate way layer.
+This code will read data from dyanmod db based on tableName request, and pass data to API Gateway.
 */
+
+// declare and initialize the aws-sdk variable.
 var AWS = require("aws-sdk");
-// declare and initialize dynamoDB database object.
+// declare and initialize the Dyanamod variable to read data from database.
 var docClient = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = (event,context, callback) => {
-    console.log("PARAMS---:" + parseInt(event.params.querystring.location));
-    
-	// pull information from specific table based on specific primary key.
+// display the parameter value that is being passed.
+console.log("PARAMS---:" + /*parseInt(*/event.params.querystring.TableName/*)*/);    
     var params = {
-        TableName : "Sustainibility_location_info",
-        Key :{
-            "location" : parseInt(event.params.querystring.location)
-        }
-    };
-    
-	// display error if data is invalid.otherwise, display result.
-    docClient.get(params, function(err,data){
+        // define the table to display the output.
+         "TableName" : event.params.querystring.TableName         
+         };
+
+// scan the table and display whole table, or print error message if table doesn't exist.
+    docClient.scan(params, function(err,data){
         if(err)
         {
-            console.error("Unable to read location. Error JSON:" , JSON.stringify(err,null,2));
+            console.error("Unable to read Id. Error JSON:" , JSON.stringify(err,null,2));
         }
         else
         {
             callback(null,JSON.parse(JSON.stringify(data,null,2)));
         }
     });
+    
 };
-
-// path queryset up for testing.
-{
-
-"body-json":{}
-"params":{
-	"path":{}
-	"querystring":{
-		"location":"1"
-	}
-}
-}
