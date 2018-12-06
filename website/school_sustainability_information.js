000fc22a4ba42cb3394f55ca0446d33896d4750c
mdpatel4 <31790430+mdpatel4@users.noreply.github.com>
2018-12-06 02:59:12 -0800

    Add files via upload

---
+++ website/school_sustainability_information.js
@@ -0,0 +1,55 @@
+/**
+This file will get called in html file to display the table value for sustainability at School.
+
+created by Maulik Patel
+*/
+// declare and initialize variables.
+var dataResult;
+var apigClient = apigClientFactory.newClient();
+
+// sustainableHome: displays the sustainable tip for school.
+function sustainableSchool(){
+// pass the table name as parameter.
+var formData = {
+'TableName': "eco_tip"
+};
+
+var params = {'TableName': "eco_tip",
+};
+var body = {
+
+};
+var additionalParams = {
+headers :{},
+queryParams: {'TableName': "eco_tip"
+}
+};
+
+var resultStr = "<ol>";
+
+// call apiGateway to request the information from database and print it.
+apigClient.ecowebresourceGet(params, body, additionalParams)
+.then(function(result){
+dataResult = result.data;
+
+// iterate values from object.
+result.data.Items.forEach(function(item){
+	// if location value is equal to School than display sustainability tip. otherwise skip.
+  if((item.place).localeCompare("school")==0){
+	  // save result on variable.
+    resultStr =   resultStr + "<br><li>" +  item.tip + "</li>";
+    }
+	// return result.
+    return resultStr + "</ol>";
+  });
+// go to exception if data doesn't exist in database.
+}).catch(function (result){
+	// save error message.
+  resultStr = "data not available";
+  });
+  // return result.
+  return resultStr;
+};
+
+// display result on website.
+document.getElementById("demo").innerHTML = sustainableSchool();
